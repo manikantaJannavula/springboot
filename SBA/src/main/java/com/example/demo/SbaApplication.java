d991ff786c888b4892bc33daf7d7592fc3213267
Manikanta-astragrown <svram930@gmail.com>
2025-02-11 14:32:52 +0530

    Day 9

---
+++ SBA/src/main/java/com/example/demo/SbaApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbaApplication.class, args);
+
+        // Check Tomcat Status After Spring Boot Starts
+        TomcatCheckerController checker = new TomcatCheckerController();
+        if (checker.isTomcatRunning()) {
+            System.out.println("Tomcat is running on port 8080.");
+        } else {
+            System.out.println("Tomcat is NOT running.");
+            
+            
+        }
+    }
+}
